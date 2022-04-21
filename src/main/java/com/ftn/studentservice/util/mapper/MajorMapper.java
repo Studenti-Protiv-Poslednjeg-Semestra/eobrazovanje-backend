caed17fce957aee288aadd6b72d60e7d79216c35
Konstantin Lazarevic <konstantin42@programmer.net>
2022-04-21 17:56:19 +0200

    update majormapper

--- src/main/java/com/ftn/studentservice/util/mapper/MajorMapper.java
+++ src/main/java/com/ftn/studentservice/util/mapper/MajorMapper.java
@@ -4,10 +4,17 @@ import com.ftn.studentservice.model.Major;
 import com.ftn.studentservice.web.dto.MajorDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MajorMapper {
 
     MajorDTO toDto(Major major);
+
+    // TODO: Dopunite ovo, sta god to bilo. Trazio je da implementiram iz EntityMapper-a.
+    List<Major> toEntity(List<MajorDTO> dtoList);
+
+    List<MajorDTO> toDto(List<Major> entityList);
 
     Major toEntity(MajorDTO majorDTO);
 }
