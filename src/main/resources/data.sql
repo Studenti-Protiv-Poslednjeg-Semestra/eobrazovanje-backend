53295a06158fb41b10db80f58f30c81e3816b685
Konstantin Lazarevic <konstantin42@programmer.net>
2022-04-22 15:01:57 +0200

    update data.sql
    This will all change when we switch to separate sql file way of inserting data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
 INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('1', 'ROLE_ADMIN');
-INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('2', 'ROLE_PROFESSOR');
-INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('3', 'ROLE_ASSISTANT');
-INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('4', 'ROLE_DEMONSTRATOR');
-INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('5', 'ROLE_STUDENT');
+INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('2', 'ROLE_TEACHER');
+INSERT INTO `student-service`.`authority` (`id`, `name`) VALUES ('3', 'ROLE_STUDENT');
